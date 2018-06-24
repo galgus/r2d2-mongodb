fb77889ede421301e0ae54b5a7dea3c94e083daf
Peter Majchrak <petoknm@gmail.com>
2018-06-24 21:36:16 +0200

    v0.1.2 - Some documentation, tiny API changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![crates.io](https://img.shields.io/crates/v/r2d2-mongodb.svg)](https://crates.io/crates/r2d2-mongodb)
-[![docs.rs](https://img.shields.io/badge/docs.rs-r2d2--mongodb-green.svg)](https://docs.rs/r2d2-mongodb)
+[![docs.rs](https://docs.rs/r2d2-mongodb/badge.svg)](https://docs.rs/r2d2-mongodb)
 
 # r2d2-mongodb
 
@@ -16,22 +16,24 @@ extern crate r2d2;
 extern crate r2d2_mongodb;
 
 use r2d2::Pool;
-use r2d2_mongodb::MongodbConnectionManagerBuilder;
+use r2d2_mongodb::{ConnectionOptionsBuilder, MongodbConnectionManager};
 
 fn main () {
-    let manager = MongodbConnectionManagerBuilder::new()
-        .with_host("localhost")
-        .with_port(27017)
-        .with_db("admin")
-        .with_username("root")
-        .with_password("password")
-        .build();
+    let manager = MongodbConnectionManager::new(
+        ConnectionOptionsBuilder::new()
+            .with_host("localhost")
+            .with_port(27017)
+            .with_db("admin")
+            .with_username("root")
+            .with_password("password")
+            .build()
+    );
 
     let pool = Pool::builder()
         .max_size(64)
         .build(manager)
         .unwrap();
 
-    ...
+    //...
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! # r2d2-mongodb
+//! A MongoDB adaptor for r2d2 connection pool.
+//! # Example
+//! ```rust
+//! extern crate r2d2;
+//! extern crate r2d2_mongodb;
+//!
+//! use r2d2::Pool;
+//! use r2d2_mongodb::{ConnectionOptionsBuilder, MongodbConnectionManager};
+//!
+//! fn main () {
+//!     let manager = MongodbConnectionManager::new(
+//!         ConnectionOptionsBuilder::new()
+//!             .with_host("localhost")
+//!             .with_port(27017)
+//!             .with_db("admin")
+//!             .with_username("root")
+//!             .with_password("password")
+//!             .build()
+//!     );
+//!
+//!     let pool = Pool::builder()
+//!         .max_size(64)
+//!         .build(manager)
+//!         .unwrap();
+//!
+//!     //...
+//! }
+//! ```
+
 pub extern crate mongodb;
 pub extern crate r2d2;
 
@@ -5,13 +35,31 @@ use r2d2::ManageConnection;
 use mongodb::{ThreadedClient, Client, Error};
 use mongodb::db::{ThreadedDatabase, Database};
 
+/// Options with which the connections to MongoDB will be created
+///
+/// To authenticate the connection you have to provide both a `username` and `password`.
 #[derive(Clone)]
 pub struct ConnectionOptions {
-    host: String,
-    port: u16,
-    db: String,
-    username: Option<String>,
-    password: Option<String>,
+    /// Address of the MongoDB server
+    ///
+    /// Default: `"localhost"`
+    pub host: String,
+    /// Port on which to connect
+    ///
+    /// Default: `27017`
+    pub port: u16,
+    /// Name of the database to connect to
+    ///
+    /// Default: `"admin"`
+    pub db: String,
+    /// Username for authentication
+    ///
+    /// Default: `None`
+    pub username: Option<String>,
+    /// Password for authentication
+    ///
+    /// Default: `None`
+    pub password: Option<String>,
 }
 
 impl Default for ConnectionOptions {
@@ -26,51 +74,55 @@ impl Default for ConnectionOptions {
     }
 }
 
-pub struct MongodbConnectionManagerBuilder {
-    options: ConnectionOptions,
-}
+/// Builder for `ConnectionOptions`
+pub struct ConnectionOptionsBuilder(ConnectionOptions);
 
-impl MongodbConnectionManagerBuilder {
-    pub fn new() -> MongodbConnectionManagerBuilder {
-        MongodbConnectionManagerBuilder{
-            options: ConnectionOptions::default()
-        }
+impl ConnectionOptionsBuilder {
+    pub fn new() -> ConnectionOptionsBuilder {
+        ConnectionOptionsBuilder(ConnectionOptions::default())
     }
 
-    pub fn with_host<'a>(&'a mut self, host: &str) -> &'a mut MongodbConnectionManagerBuilder {
-        self.options.host = host.to_string();
+    pub fn with_host<'a>(&'a mut self, host: &str) -> &'a mut ConnectionOptionsBuilder {
+        self.0.host = host.to_string();
         self
     }
 
-    pub fn with_port<'a>(&'a mut self, port: u16) -> &'a mut MongodbConnectionManagerBuilder {
-        self.options.port = port;
+    pub fn with_port<'a>(&'a mut self, port: u16) -> &'a mut ConnectionOptionsBuilder {
+        self.0.port = port;
         self
     }
 
-    pub fn with_db<'a>(&'a mut self, db: &str) -> &'a mut MongodbConnectionManagerBuilder {
-        self.options.db = db.to_string();
+    pub fn with_db<'a>(&'a mut self, db: &str) -> &'a mut ConnectionOptionsBuilder {
+        self.0.db = db.to_string();
         self
     }
 
-    pub fn with_username<'a>(&'a mut self, username: &str) -> &'a mut MongodbConnectionManagerBuilder {
-        self.options.username = Some(username.to_string());
+    pub fn with_username<'a>(&'a mut self, username: &str) -> &'a mut ConnectionOptionsBuilder {
+        self.0.username = Some(username.to_string());
         self
     }
 
-    pub fn with_password<'a>(&'a mut self, password: &str) -> &'a mut MongodbConnectionManagerBuilder {
-        self.options.password = Some(password.to_string());
+    pub fn with_password<'a>(&'a mut self, password: &str) -> &'a mut ConnectionOptionsBuilder {
+        self.0.password = Some(password.to_string());
         self
     }
 
-    pub fn build(&mut self) -> MongodbConnectionManager {
-        MongodbConnectionManager{
-            options: self.options.clone()
-        }
+    pub fn build(&mut self) -> ConnectionOptions {
+        self.0.clone()
     }
 }
 
+/// Struct for managing a pool of MongoDB connections
 pub struct MongodbConnectionManager {
     options: ConnectionOptions,
+}
+
+impl MongodbConnectionManager {
+    pub fn new(options: ConnectionOptions) -> MongodbConnectionManager {
+        MongodbConnectionManager {
+            options
+        }
+    }
 }
 
 impl ManageConnection for MongodbConnectionManager {
